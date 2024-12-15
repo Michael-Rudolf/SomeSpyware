9a7de6a6606169f0b34870e3a877ecd3a7b0db98
Michael Rudolf <michael.rudolf85@gmail.com>
2024-12-15 18:48:20 +0100

    IDK

--- nnnano.py
+++ nnnano.py
@@ -1,14 +1,11 @@
 import sys
-import firebase_admin
-from firebase_admin import credential, firestore, storage
-
-cred = credentials.Certificate
+import firebase
 # Set a default value for the file_text
 file_text = ""
 
 print("length: " + str(len(sys.argv)))
 if len(sys.argv) > 1:
-    print("arguments provided" + sys.argv[1])
+    print("arguments provided:" + sys.argv[1])
     with open(sys.argv[1]) as file:
         file_text = file.read()
     print(file_text)
