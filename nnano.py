e6efdde9d7f5e6968d8467fe74d6fa6606c1cdee
Michael Rudolf <michael.rudolf85@gmail.com>
2024-12-24 15:22:54 +0100

    Removed unecessary debug information

--- nnano.py
+++ nnano.py
@@ -1,15 +1,13 @@
 import sys
 import os
-
+import firebase_admin
 # Set a default value for the file_text
 file_text = ""
 
 print("length: " + str(len(sys.argv)))
 if len(sys.argv) > 1:
-    print("arguments provided:" + sys.argv[1])
     with open(sys.argv[1]) as file:
         file_text = file.read()
-    print(file_text)
     
     os.system("echo DebugInfo: Python code executed successfully.")
     os.system("nano " + sys.argv[1])
